7547bcb6a0c15b462b7dc5f36d5f693503168a87
Jorge Martinez <jorgemartinez1990@gmail.com>
2022-09-01 11:38:35 -0400

    algosess

---
+++ test.js
@@ -0,0 +1,8 @@
+var peopleArr =[
+    {"firstName":"Jane", "lastName":"Doe", "age":45, "hairColor":"Black"},
+    {"firstName":"John", "lastName":"Smith","age":88,"hairColor":"Brown"},
+    {"firstName":"Millard", "lastName":"Fillmore","age":50,"hairColor":"Brown"},
+    {"firstName":"Maria", "lastName":"Smith","age":62,"hairColor":"Brown"}
+  ]
+
+peopleArr.map()
