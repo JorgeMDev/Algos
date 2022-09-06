1550c4023f9667b0b041d267a50930a31dd90028
Jorge Martinez <jorgemartinez1990@gmail.com>
2022-09-06 12:02:12 -0400

    1

--- unionIntersection.js
+++ unionIntersection.js
@@ -87,7 +87,33 @@ const expected5 = [1, 2, 2, 2, 6, 6, 7, 10, 15, 20];
  *    not the combined amount from both sets.
  */
 // function orderedMultisetUnion(sortedA, sortedB) {}
-
+function orderedUnion(sortedA, sortedB) {
+  let newArr = []
+  let ai = 0 
+  let bi = 0 
+  let arri = 0
+  while (ai<sortedA.length && bi<sortedB.length){
+    if(sortedA[ai]<sortedB[bi]){
+      newArr.push(sortedA[ai]) 
+      ai++
+    }
+    else if (sortedA[ai]>sortedB[bi]){
+      newArr.push(sortedB[bi]) 
+      bi++
+    }
+    else{ //if they are equal add to expected arr
+      newArr.push(sortedA[ai]) 
+      arri++
+      ai++
+      bi++
+      // if (newArr[arri] === newArr[arri+1]){ //check for duplicates
+      //   newArr.pop()
+      //   arri++
+      // }
+    }
+  }
+  return newArr
+}
 
 
 // bonus algo: 
